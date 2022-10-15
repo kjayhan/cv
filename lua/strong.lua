7559260f7fe84a7aae16634ed69456bfc0b67ca5
Kadir Jun Ayhan <88925612+kjayhan@users.noreply.github.com>
2022-10-15 14:49:59 +0900

    highlighted name (except J.)

--- lua/strong.lua
+++ lua/strong.lua
@@ -1,25 +1,21 @@
-local highlight_author_filter = {
-  Para = function(el)
-    if el.t == "Para" then
-    for k,_ in ipairs(el.content) do
-      if el.content[k].t == "Str" and el.content[k].text == "Ayhan,"
-      and el.content[k+1].t == "Space"
-      and el.content[k+2].t == "Str" and el.content[k+2].text:find("^K. J.") then
-          local _,e = el.content[k+2].text:find("^K. J.")
-          local rest = el.content[k+2].text:sub(e+1) 
-          el.content[k] = pandoc.Strong { pandoc.Str("Ayhan, K. J.") }
-          el.content[k+1] = pandoc.Str(rest)
-          table.remove(el.content, k+2) 
-      end
-    end
-  end
-  return el
-  end
-}
+function Block(el)
+    if el.t == "Para" or el.t == "Plain" then
+        for k, _ in ipairs(el.content) do
 
-function Div (div)
-  if 'refs' == div.identifier then
-    return pandoc.walk_block(div, highlight_author_filter)
-  end
-  return nil
+            if el.content[k].t == "Str" and el.content[k].text == "Ayhan," and
+                el.content[k + 1].t == "Space" and el.content[k + 2].t == "Str" and
+                el.content[k + 2].text:find("^K.") then
+
+                local _, e = el.content[k + 2].text:find("^K.")
+                local rest = el.content[k + 2].text:sub(e + 1) -- empty if e+1>length
+                el.content[k] = pandoc.Strong {pandoc.Str("Ayhan, K.")}
+                el.content[k + 1] = pandoc.Str(rest)
+                table.remove(el.content, k + 2) -- safe? another way would be to set element k+2 to Str("")
+                -- no real need to skip ipairs items here
+
+            end
+
+        end
+    end
+    return el
 end
